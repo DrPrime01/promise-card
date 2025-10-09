9318b29107dfb69c8e702a84fa2fbdd976cc14c2
DrPrime01 <drprime.dev@gmail.com>
2025-10-09 15:52:41 +0100

    chore: updated next-auth installation

--- auth.ts
+++ auth.ts
@@ -1,8 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import AppleProvider from "next-auth/providers/apple";
-import FacebookProvider from "next-auth/providers/facebook";
-import { BASE_URL } from "./constants";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
@@ -14,47 +11,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-    }),
-    AppleProvider({
-      clientId: process.env.APPLE_ID!,
-      clientSecret: process.env.APPLE_SECRET!,
-    }),
   ],
   callbacks: {
-    async signIn({ account, profile }) {
-      if (account?.provider === "google") {
-        try {
-          // Here, you have access to the user's data from Google.
-          console.log("Google User Profile:", profile);
-          console.log("Google User Account:", account);
-
-          const response = await fetch(`${BASE_URL}/auth/google-auth`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              payload: account.access_token,
-            }),
-          });
-
-          if (!response.ok) {
-            console.error("Failed to sync user with backend");
-            return false;
-          }
-
-          // Returning true allows the sign-in process to continue.
-          return true;
-        } catch (error) {
-          console.error("Error during custom Google sign-in logic:", error);
-
-          return false;
-        }
-      }
-      // Allow other providers to sign in.
-      return true;
-    },
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
